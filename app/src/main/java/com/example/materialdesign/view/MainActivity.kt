463fc07013600f9d27aaaff8654093476174dae0
татьяна <emuart155@gmail.com>
2021-09-21 15:24:47 +0300

    Добавила SplashActivity для отображения иконки и запуска основного экрана MainActivity

--- app/src/main/java/com/example/materialdesign/view/MainActivity.kt
+++ app/src/main/java/com/example/materialdesign/view/MainActivity.kt
@@ -20,9 +20,10 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: MainActivityBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
 
         setTheme(ThemeHolder.theme)
+
+        super.onCreate(savedInstanceState)
 
         binding = MainActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
